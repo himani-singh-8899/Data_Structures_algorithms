7ab46fb60d6e178701a973a1f5c4b9d73bc4975f
himani-singh-8899 <63301803+himani-singh-8899@users.noreply.github.com>
2021-05-07 17:34:11 +0530

    Update reverse_array_in_groups.cpp
    Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.

--- Arrays/reverse_array_in_groups.cpp
+++ Arrays/reverse_array_in_groups.cpp
@@ -1,3 +1,9 @@
+/*Input:
+N = 5, K = 3
+arr[] = {1,2,3,4,5}
+Output: 3 2 1 5 4
+Explanation: First group consists of elements
+1, 2, 3. Second group consists of 4,5.*/
 class Solution{
 public:
     //Function to reverse every sub-array group of size k.
