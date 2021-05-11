18aef3f29795ab17719dcac9d0175a8e15756db2
himani-singh-8899 <63301803+himani-singh-8899@users.noreply.github.com>
2021-05-12 00:30:30 +0530

    Update bubblesort.cpp

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -18,15 +18,23 @@ Third Pass:
 ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) */
 void bubblesort(int arr[],int n)
 {
-  for(int i=0;i<n;i++)
-  {
-    for(int j=0;j<n-i-1;j++)
-    {
-      if(arr[j]>arr[j+1])
-      {
-        swap(&arr[i],&arr[j];
-       }
-    }
-  }
+  int i, j;
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(&arr[j], &arr[j+1]);
+           swapped = true;
+        }
+     }
+ 
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
+   }
   
 }
